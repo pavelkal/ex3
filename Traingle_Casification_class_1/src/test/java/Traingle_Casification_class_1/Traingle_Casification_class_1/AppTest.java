b8d47d3d0e2ae0fc82cc91aff21d1f694f6c8df7
паша <@LAPTOP-ROBAKT4L>
2024-02-03 17:52:17 +0200

    finish

--- Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
+++ Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
@@ -11,10 +11,10 @@ import  org.junit.Test;
 public class AppTest {
 	
 	TriangleClassifier triangle = new TriangleClassifier();
-	
+	//
 	@Test
   
-	public void equilateralTriangle(){
+	public void equilateralTriangle1(){
         int a = 2;
         int b = 2;
         int c=2;
